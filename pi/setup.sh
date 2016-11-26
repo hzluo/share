bb975a573475bfc29ff83fe974e3a5ef77936771
Hongzhang Luo <lhz1983001@gmail.com>
2016-11-26 14:42:44 +0800

    安装中文语言包

--- pi/setup.sh
+++ pi/setup.sh
@@ -1,1 +1,7 @@
 sudo cp sources.list /etc/apt/sources.list
+sudo apt-get install ttf-wqy-zenhei
+sudo apt-get install wget git-core
+
+sudo apt-get install ssh
+sudo /etc/init.d/ssh start
+sudo update-rc.d ssh defaults
